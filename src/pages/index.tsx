558470599326aaa88ff54435e1ad42705f285770
Benjamin Belpoliti <belpolitibenja@gmail.com>
2023-01-07 05:41:46 -0300

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,8 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "../utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.example.test.useQuery();
+  console.log(data);
 
   return (
     <>
@@ -46,7 +47,7 @@ const Home: NextPage = () => {
           </div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+              {/* {hello.data ? hello.data.greeting : "Loading tRPC query..."} */}
             </p>
             <AuthShowcase />
           </div>
@@ -63,7 +64,7 @@ const AuthShowcase: React.FC = () => {
 
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined, // no input
-    { enabled: sessionData?.user !== undefined },
+    { enabled: sessionData?.user !== undefined }
   );
 
   return (
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -10,12 +10,14 @@ export const exampleRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
-
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.example.findMany();
   }),
-
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
+  }),
+  test: publicProcedure.query(async () => {
+    const res = await fetch(`https://www.dnd5eapi.co/api/ability-scores/cha`);
+    return res.json();
   }),
 });
